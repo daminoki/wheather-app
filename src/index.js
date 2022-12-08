fad372e99d51f7af3e0cbc0662af609e7d42d569
Daria Andreeva <dd1556@mail.ru>
2022-12-08 16:17:36 +0300

    section header added

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=e6970efb880b105e85f3508dd47a2c23')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+
+// const fetchData = async () => {
+//     const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=e6970efb880b105e85f3508dd47a2c23'
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data)
+// }
+
+// fetchData();
+
