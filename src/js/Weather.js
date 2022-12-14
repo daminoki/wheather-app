2a81a281da03370878faf72fcf973382ce0cba8a
Daria Andreeva <dd1556@mail.ru>
2022-12-14 12:41:44 +0300

    add setTime function to Weather class

--- src/js/Weather.js
+++ src/js/Weather.js
@@ -21,16 +21,20 @@ export default class Weather {
     this._cityName = document.querySelector('.main-information__city');
     this._weatherIcon = document.querySelector('.main-information__weather-icon');
     this._date = document.querySelector('.main-information__current-date');
+    this._time = document.querySelector('.main-information__current-time');
     this._temp = document.querySelector('.main-information__temp');
     this._weatherDescription = document.querySelector('.main-information__weather-description');
     this._visibility = document.querySelector('.widgets__main_description-visibility');
+    this._wind = document.querySelector('.widgets__main_description-wind');
+    this._humidity = document.querySelector('.widgets__main_description-humidity');
+    this._feelsLike = document.querySelector('.widgets__main_description-feels-like');
+    this._maxTemp = document.querySelector('.main-information__temp-diff_highest');
+    this._minTemp = document.querySelector('.main-information__temp-diff_lowest');
   }
 
   async init() {
     window.addEventListener('itemSelectedEvent', async (e) => {
       const { selectedItem } = e.detail;
-
-      console.log(selectedItem);
 
       await Promise.all(
         [
@@ -39,8 +43,8 @@ export default class Weather {
         ],
       );
 
-      this._setData();
-      console.log(123, this._currentWeatherData.dt, this._currentWeatherData.timezone);
+      this._setСurrentData();
+      this._setTime(selectedItem);
     });
   }
 
@@ -56,20 +60,34 @@ export default class Weather {
     this._forecastWeatherData = await this._api(this._forecastWeatherUrl);
   }
 
-  _setData() {
+  _setСurrentData() {
     this._cityName.textContent = this._currentWeatherData.name;
     this._weatherIcon.src = `http://openweathermap.org/img/wn/${this._currentWeatherData.weather[0].icon}@2x.png`;
-    // const fullDate = new Date((this._currentWeatherData.dt
-    //   + this._currentWeatherData.timezone) * 1000);
-    // const fullDate1 = new Date(fullDate).toISOString();
-    // console.log(fullDate1);
-    // const options = {
-    //   weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',
-    // };
-    // this._date.textContent = new Intl.DateTimeFormat('en-US', options).format(fullDate1);
     this._temp.textContent = `${String(this._currentWeatherData.main.temp).split('.')[0]}°`;
     this._weatherDescription.textContent = this._currentWeatherData.weather[0].description;
     this._weatherDescription.textContent = `${this._weatherDescription.textContent[0].charAt(0).toUpperCase()}${this._weatherDescription.textContent.slice(1)}`;
     this._visibility.textContent = `${this._currentWeatherData.visibility / 1000} km`;
+    this._wind.textContent = `${String(this._currentWeatherData.wind.speed).split('.')[0]} m/s`;
+    this._humidity.textContent = `${this._currentWeatherData.main.humidity}%`;
+    this._feelsLike.textContent = `${String(this._currentWeatherData.main.feels_like).split('.')[0]}°`;
+    this._maxTemp.textContent = `H: ${String(this._currentWeatherData.main.temp_max).split('.')[0]}°`;
+    this._minTemp.textContent = `L: ${String(this._currentWeatherData.main.temp_max).split('.')[0]}°`;
+  }
+
+  _setTime(selectedItem) {
+    const fullDate = new Date(this._currentWeatherData.dt * 1000);
+    const dateOptions = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      timeZone: selectedItem.timezone_module.name,
+    };
+    const timeOptions = {
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZone: selectedItem.timezone_module.name,
+    };
+    this._date.textContent = new Intl.DateTimeFormat('en-US', dateOptions).format(fullDate);
+    this._time.textContent = new Intl.DateTimeFormat('ru-RU', timeOptions).format(fullDate);
   }
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,60 @@
 export const cityList = [
-  { title: 'Kirov', lat: 58.59665, lon: 49.66007 },
-  { title: 'Karaganda', lat: 49.83333, lon: 73.1658 },
-  { title: 'Kazan, Russia', lat: 55.796391, lon: 49.108891 },
-  { title: 'Kazan, Turkey', lat: 40.205166, lon: 32.681183 },
-  { title: 'Kumeny', lat: 58.10887, lon: 49.91614 },
-  { title: 'Kostroma', lat: 57.767193, lon: 40.976257 },
-  { title: 'Korston', lat: 43.1056, lon: 131.874 },
+  {
+    title: 'Kirov',
+    lat: 58.59665,
+    lon: 49.66007,
+    timezone_module: {
+      name: 'Europe/Moscow',
+    },
+  },
+  {
+    title: 'Karaganda',
+    lat: 49.83333,
+    lon: 73.1658,
+    timezone_module: {
+      name: 'Asia/Almaty',
+    },
+  },
+  {
+    title: 'Kazan, Russia',
+    lat: 55.796391,
+    lon: 49.108891,
+    timezone_module: {
+      name: 'Europe/Moscow',
+    },
+  },
+  {
+    title: 'Kazan, Turkey',
+    lat: 40.205166,
+    lon: 32.681183,
+    timezone_module: {
+      name: 'Europe/Istanbul',
+    },
+  },
+  {
+    title: 'Kumeny',
+    lat: 58.10887,
+    lon: 49.91614,
+    timezone_module: {
+      name: 'Europe/Moscow',
+    },
+  },
+  {
+    title: 'Kostroma',
+    lat: 57.767193,
+    lon: 40.976257,
+    timezone_module: {
+      name: 'Europe/Moscow',
+    },
+  },
+  {
+    title: 'Korston',
+    lat: 43.1056,
+    lon: 131.874,
+    timezone_module: {
+      name: 'Asia/Vladivostok',
+    },
+  },
 ];
 
 export const listItem = (title) => `<button class="search__dropdown-button">${title}</button>`;
